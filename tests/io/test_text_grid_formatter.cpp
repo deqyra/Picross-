37b6a9cde48452b092a179998a045b1234ae0044
François Brachais <deqyra@gmail.com>
2019-11-03 04:40:50 +0100

    CircleCI and Appveyor stubs

--- tests/io/test_text_grid_formatter.cpp
+++ tests/io/test_text_grid_formatter.cpp
@@ -19,5 +19,6 @@ namespace Picross
         g = generate10x10PartialGrid();
         expected = StringUtil::readFileIntoString("resources/tests/io/10_10_partial_formatted_with_hints.txt");
         REQUIRE(txt.renderGridWithHints(g) == expected);
+        Catch::cout() << txt.renderGridWithHints(g);
     }
 }
