39b3da2769663ddc779a5e4c1e9d9ccca90ed9ef
François Brachais <deqyra@gmail.com>
2019-11-04 14:17:25 +0100

    Add simple exception generator macro

--- io/invalid_xml_grid_error.cpp
+++ io/invalid_xml_grid_error.cpp
@@ -2,20 +2,5 @@
 
 namespace Picross
 {
-    InvalidXMLGridError::InvalidXMLGridError(const char* message) :
-        _message(message)
-    {
-
-    }
-
-    InvalidXMLGridError::InvalidXMLGridError(std::string message) :
-        _message(message)
-    {
-
-    }
-    
-    const char* InvalidXMLGridError::what() const noexcept
-    {
-        return _message.c_str();
-    }
+    DEFINE_BASIC_EXCEPTION(InvalidXMLGridError)
 }
--- io/invalid_xml_grid_error.hpp
+++ io/invalid_xml_grid_error.hpp
@@ -1,21 +1,11 @@
 #ifndef IO__INVALID_XML_GRID_ERROR
 #define IO__INVALID_XML_GRID_ERROR
 
-#include <stdexcept>
-#include <string>
+#include "../make_basic_exception.hpp"
 
 namespace Picross
 {
-    class InvalidXMLGridError : public std::exception
-    {
-        private:    // Attributes
-            std::string _message;
-
-        public:     // Public methods
-            InvalidXMLGridError(const char* message);
-            InvalidXMLGridError(std::string message);
-            const char* what() const noexcept;
-    };
+    DECLARE_BASIC_EXCEPTION(InvalidXMLGridError)
 }
 
 #endif//IO__INVALID_XML_GRID_ERROR
